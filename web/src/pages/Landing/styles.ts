1288ecaf96a65f71db80e91b20739f3661a63193
unknown <thiago.fidelis27@gmail.com>
2020-08-22 07:24:28 -0300

    add responsive mobile

--- web/src/pages/Landing/styles.ts
+++ web/src/pages/Landing/styles.ts
@@ -133,7 +133,7 @@ export const ButtonsContainer = styled.div`
       height: 8.4rem;
     }
     padding: 0 2rem;
-    width: 20rem;
+    width: 18rem;
     height: 6.4rem;
     border-radius: 0.8rem;
     margin-right: 1.6rem;
